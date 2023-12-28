1bd7d6bc928f074eea60aad4f9bdbe193c1a0fbc
shira-apiiro <145326526+shira-apiiro@users.noreply.github.com>
2023-12-28 13:56:50 +0200

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -3,3 +3,5 @@ import requests
 password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
 
 response = requests.get('http://google.com', {'password': password})
+
+Token: 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
