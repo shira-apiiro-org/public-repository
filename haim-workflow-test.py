7f6ad62605c68ef2993ed1ba08d5f930bf90e814
shira-apiiro <145326526+shira-apiiro@users.noreply.github.com>
2023-12-31 16:06:19 +0200

    Update haim-workflow-test.py

--- haim-workflow-test.py
+++ haim-workflow-test.py
@@ -1,6 +1,7 @@
 import requests
 
 pwd = "AIzaSyDzmABOfcTbibcP_VnZPhFn0q3Ro3sXlSQ"
+password = "AIzaSyDzmABOfcTbibcP_VnZPsFn0q2Ro2sXlSQ"
 
 res = requests.get('http://google.com', {'password': pwd})
 Token: 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
