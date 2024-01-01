64fe4330974aa712a004beccdccaec4ed5f32929
shira-apiiro <145326526+shira-apiiro@users.noreply.github.com>
2024-01-01 17:54:11 +0200

    Update haim-test.py

--- haim-test.py
+++ haim-test.py
@@ -7,3 +7,4 @@ pass = "AIzaSyCzmABOfcTbibcP_VnZPsFn0q2Ro2sXlHQ"
 pass = "BIzbSeCznABOfcTbibaP_VmZPsFn0q1Ro3sXlHQ"
 
 res = requests.get('http://google.com', {'password': pass})
+token= "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"
