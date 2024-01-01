66d4b9c2106dd066a5cb1940860b9c90f58623c8
shira-apiiro <145326526+shira-apiiro@users.noreply.github.com>
2024-01-01 13:58:36 +0200

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -11,3 +11,4 @@ Token: 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
 pass = "AIzaSyDzmABOfcTbibcP_VnZPsFn0q2Ro2sXlSQ"
 
 pass = "AIzaSyDzmABOfcTbibcP_VnZPsFn0q2Ro1iGuHS"
+token= "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"
