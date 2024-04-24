3f41b31993eb37867c17903a05b41feeef1833ec
shira-apiiro <145326526+shira-apiiro@users.noreply.github.com>
2024-04-24 16:37:04 +0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -16,3 +16,4 @@ token= "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"
 AUTOFILL_HINT_WIFI_PASSWORD = "Password"
 
 pwd = "AIzaSyDssABOfcTbibaP_VnZPhFn0q3Ro2sXsSQ"
+pwd = "AIzaSyDssABOfcTbibaP_VnZPhFn0q3Ro2sShSQ"
