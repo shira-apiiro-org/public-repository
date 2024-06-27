daf25070882812fa60ec95370939453c8484e800
shira-apiiro <145326526+shira-apiiro@users.noreply.github.com>
2024-06-27 11:37:57 +0300

    Update haim-test.py

--- haim-test.py
+++ haim-test.py
@@ -1,10 +1,11 @@
 import requests
 
 Token: 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
+token= "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"
+
+Password = AIzaSyCzmABOfcTbibaP_ZmZPsFn0q3Ro2sXlHQ
 
 pass = "AIzaSyCzmABOfcTbibcP_VnZPsFn0q2Ro2sXlHQ"
-
 pass = "BIzbSeCznABOfcTbibaP_VmZPsFn0q1Ro3sXlHQ"
 
 res = requests.get('http://google.com', {'password': pass})
-token= "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"
