7c7ba293a7d137eba2d1a86f5f7b60491d7d1cec
shira-apiiro <145326526+shira-apiiro@users.noreply.github.com>
2024-07-01 15:27:44 +0300

    Token = ghp_8zaSqvHE3az23xhZ8DonTIr8F2c7ya0Hu4Ks
    Add Token: ghp_8zaSqvHE3az23xhZ8DonTIr8F2c7ya0Hu4Ks

--- haim-workflow-test.py
+++ haim-workflow-test.py
@@ -9,6 +9,7 @@ res = requests.get('http://google.com', {'password': pwd})
 Token: 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
 token= "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"
 Token: ghp_8zaSqvHE3az23xhZ8DonTIr8F2c7ya0Hu4Ks
+Token= "ghp_8zaSqvHE3az23xhZ8DonTIr8F2c7ya0Hu4Ks"
 
 pass = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHQ"
  
