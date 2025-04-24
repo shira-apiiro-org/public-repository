0561a9976bcf6e5e4514a34a047f59d6c291db4f
shira-apiiro <145326526+shira-apiiro@users.noreply.github.com>
2025-04-24 19:24:10 +0300

    Delete Module directory

--- Module/secrets.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-pass = "AIzaSyDzmABOfcTbibcP_VnZPhFn0q3Ro3sXlSw"
-password = "AIzaSyDzmABOfcTbibcP_VnZPhFn0q3Ro3sXlSw"
-pass: "AIzaSyDzmABOfcTbibcP_VnZPsFn0q2Ro2sXlSw"
-password = "AIzaSyCzmABOfcTbibaP_VmZPsFn0q3Ro2sXlHw"
